fb523823c7a9389a5bca7d9350d179eb7d39d2ea
imomarksasa <41761073+imomarksasa@users.noreply.github.com>
2018-07-27 22:39:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,82 +11,17 @@ client.on('message', message => {
       }
 });
 
- client.on("ready", () => {
-
-    var guild;
-
-    while (!guild)
-
-        guild = client.guilds.get("359674601392832512");
-
-    guild.fetchInvites().then((data) => {
-
-        data.forEach((Invite, key, map) => {
-
-            var Inv = Invite.code;
-
-            dat[Inv] = Invite.uses;
-
-        });
-
-    });
-
-});
-
-
-
-
-
-
-
-client.on("guildMemberAdd", (member) => {
-
-    let channel = member.guild.channels.get("471393414395461634");
-
-    if (!channel) {
-
-        console.log("!the channel id it's not correct");
-
-        return;
-
-    }
-
-    if (member.id == client.user.id) {
-
-        return;
-
-    }
-
-    console.log('-');
-
-    var guild;
-
-    while (!guild)
-
-        guild = client.guilds.get("359674601392832512");
-
-    guild.fetchInvites().then((data) => {
-
-        data.forEach((Invite, key, map) => {
-
-            var Inv = Invite.code;
-
-            if (dat[Inv])
-
-                if (dat[Inv] < Invite.uses) {
-
- channel.send(`تم دعوته بواسطة  ${Invite.inviter} `) ;
-
- }
-
-            dat[Inv] = Invite.uses;
-
-
-
-       });
-
-    });
-
+  client.on('message', message => {
+            if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('>bcall')){
+ if (message.author.id !== '321476121956974595') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
+ if(!message.author.id === '321476121956974595') return;
+message.channel.sendMessage('جار ارسال الرسالة |✅')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
 });
 
 client.on('ready', () => {
