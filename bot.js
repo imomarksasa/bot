77d97eea9d71f730c47ff8b91d6a8a5369b44cfa
imomarksasa <41761073+imomarksasa@users.noreply.github.com>
2018-07-27 22:48:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,20 +24,7 @@ m.sendMessage(args)
 }
 });
 
-client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(*)) return;
 
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-  if (command == "say") {
-   message.channel.sendMessage(args.join("  "))
-   message.delete()
-  }
- });
 
 client.on('ready', () => {
   client.user.setGame(`Enjoy :)`,'https://www.twitch.tv/v5bz');
