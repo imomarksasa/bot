7f366e5f4cfb92bad144b90879e40f45d54f0ac8
imomarksasa <41761073+imomarksasa@users.noreply.github.com>
2018-07-27 22:45:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,14 +26,14 @@ m.sendMessage(args)
 
 client.on('message', message => {
   if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
+  if (!message.content.startsWith(*)) return;
 
   let command = message.content.split(" ")[0];
   command = command.slice(prefix.length);
 
   let args = message.content.split(" ").slice(1);
 
-  if (command == "*say") {
+  if (command == "say") {
    message.channel.sendMessage(args.join("  "))
    message.delete()
   }
