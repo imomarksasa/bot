a858636d46afd018ea702afc945b84849928f9d8
imomarksasa <41761073+imomarksasa@users.noreply.github.com>
2018-07-31 02:36:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,8 +17,8 @@ client.on('voiceStateUpdate', (old, now) => {
   const channel = client.channels.get('472875486700240906');
   const currentSize = channel.guild.members.filter(m => m.voiceChannel).size;
   const size = channel.name.match(/\[\s(\d+)\s\]/);
-  if (!size) return channel.setName(`Voice Online: ${currentSize}`);
-  if (currentSize !== size) channel.setName(`Voice Online: ${currentSize}`);
+  if (!size) return channel.setName(`Voice Online [ ${currentSize} ]`);
+  if (currentSize !== size) channel.setName(`Voice Online [ ${currentSize} ]`);
 });
 
 client.on('message', message => {
