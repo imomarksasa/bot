6709f98b8eada0f87da4204e1dd588551c6d9f13
imomarksasa <41761073+imomarksasa@users.noreply.github.com>
2018-08-01 11:30:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -93,33 +93,53 @@ client.on("guildMemberAdd", (member) => {
 });
 
 
-  client.on('message', message => {
-var prefix = "*";
-
-    if (message.author.id === client.user.id) return;
-    if (message.guild) {
-   let embed = new Discord.RichEmbed()
-    let args = message.content.split(' ').slice(1).join(' ');
-if(message.content.split(' ')[0] == prefix + 'bc') {
-    if (!args[1]) {
-message.channel.send("***bc <message>**");
-return;
-}
-        message.guild.members.forEach(m => {
-   if(!message.member.hasPermission('ADMINISTRATOR')) return;
-            var bc = new Discord.RichEmbed()
-            .addField('» السيرفر :', `${message.guild.name}`)
-            .addField('» المرسل : ', `${message.author.username}#${message.author.discriminator}`)
-            .addField(' » الرسالة : ', args)
-            .setColor('#ff0000')
-            // m.send(`[${m}]`);
-            m.send(`${m}`,{embed: bc});
-        });
+  client.on('message', message => {//By Codes , ' ّEpicEdiTeDّ#4968
+              if(!message.channel.guild) return;//By Codes , ' ّEpicEdiTeDّ#4968
+    var prefix = "*";//By Codes , ' ّEpicEdiTeDّ#4968
+    if(message.content.startsWith('*bc')) {//By Codes , ' ّEpicEdiTeDّ#4968
+    if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للإدارة**').then(m => m.delete(5000));//By Codes , ' ّEpicEdiTeDّ#4968
+  if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية لاستعمال هاذا الأمر** //By Codes , ' ّEpicEdiTeDّ#4968`ADMINISTRATOR`' );//By Codes , ' ّEpicEdiTeDّ#4968
+    let args = message.content.split(" ").join(" ").slice(2 + prefix.length);//By Codes , ' ّEpicEdiTeDّ#4968
+    let copy = "S Bot";//By Codes , ' ّEpicEdiTeDّ#4968
+    let request = `Requested By ${message.author.username}`;//By Codes , ' ّEpicEdiTeDّ#4968
+    if (!args) return message.reply('**يجب عليك كتابة شيئ لإرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من الإرسال؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {//By Codes , ' ّEpicEdiTeDّ#4968
+    msg.react('✅')//By Codes , ' ّEpicEdiTeDّ#4968
+    .then(() => msg.react('❌'))//By Codes , ' ّEpicEdiTeDّ#4968
+    .then(() =>msg.react('✅'))//By Codes , ' ّEpicEdiTeDّ#4968
+ 
+    let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;//By Codes , ' ّEpicEdiTeDّ#4968
+    let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;//By Codes , ' ّEpicEdiTeDّ#4968
+          let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });//By Codes , ' ّEpicEdiTeDّ#4968
+    let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });//By Codes , ' ّEpicEdiTeDّ#4968
+    reaction1.on("collect", r => {//By Codes , ' ّEpicEdiTeDّ#4968
+    message.channel.send(`**☑ |   لقد تم ارسال الرسالة لـ ${message.guild.members.size} عضوآ**`).then(m => m.delete(5000));//By Codes , ' ّEpicEdiTeDّ#4968
+    message.guild.members.forEach(m => {//By Codes , ' ّEpicEdiTeDّ#4968
+    var bc = new//By Codes , ' ّEpicEdiTeDّ#4968
+       Discord.RichEmbed()//By Codes , ' ّEpicEdiTeDّ#4968
+       .setColor('RANDOM')//By Codes , ' ّEpicEdiTeDّ#4968
+       .setDescription(`البرودكاست :mega:
+**:shield: السيرفر : ** ${message.guild.name}
+** :thinking:  المرسل : ** ${message.author.username}
+**  الرسالة : ** ${args}
+ 
+ 
+ 
+ 
+        `)//By Codes , ' ّEpicEdiTeDّ#4968
+         .setTimestamp()//By Codes , ' ّEpicEdiTeDّ#4968
+         .setFooter('S Bot' , 'https://cdn.discordapp.com/avatars/465885551329804288/55614337cfb9813916a60383469736d9.jpg?size=128')
+    m.send({ embed: bc })
+    msg.delete();//By Codes , ' ّEpicEdiTeDّ#4968
+    })//By Codes , ' ّEpicEdiTeDّ#4968
+    })//By Codes , ' ّEpicEdiTeDّ#4968
+    reaction2.on("collect", r => {//By Codes , ' ّEpicEdiTeDّ#4968
+    message.channel.send(`**تم الغاء البرودكاست :x:.**`).then(m => m.delete(5000));//By Codes , ' ّEpicEdiTeDّ#4968//By Codes , msg.delete();//By Codes , ' ّEpicEdiTeDّ#496
+    })
+    })//By Codes , ' ّEpicEdiTeDّ#4968
     }
-    } else {
-        return;
-    }
-});
+    });//By Codes , ' ّEpicEdiTeDّ#4968//By Codes , ' ّEpicEdiTeDّ#4968
+ 
+//By Codes , ' ّEpicEdiTeDّ#4968
 
 
 
