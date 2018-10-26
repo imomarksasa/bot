c8fe2be928630d006efa744a22deeee4b2b0af4c
imomarksasa <41761073+imomarksasa@users.noreply.github.com>
2018-10-26 20:57:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const fs = require('fs');
+const moment = require('moment');
+const jimp = require('jimp');
+const Canvas = require('canvas');
  const prefix = "*";
 client.on('ready', () => {
     console.log('I am ready!');
